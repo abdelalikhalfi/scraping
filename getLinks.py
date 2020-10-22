90155dbffa42c2c0fc3f898cfa90e8b008730c91
deb <38424164+debmorgan@users.noreply.github.com>
2020-10-22 22:34:40 +0100

    Update getLinks.py

--- getLinks.py
+++ getLinks.py
@@ -9,12 +9,12 @@ f = open("educ_urls", "a")
 
 for x in range(797):
 
-	trycnt = 1
+	trycnt = 3
 
 	while trycnt > 0:
 		try:
 
-			url = "https://godfgkldfg.com/d=%s" % (x)
+			url = "https://books-library.online/l-education-curriculum-best-download&ajax=1&pg=%s" % (x)
 			resp = get(url)
 
 			html_sp = BeautifulSoup(resp.text, 'html.parser')
@@ -24,9 +24,9 @@ for x in range(797):
 				f.write("https://books-library.online/%s\n" % (html_sp.find_all('div', class_='smlbooks book')[y].a['href']))
 			trycnt = 0
 
-		except:
+		except ChunkedEncodingError as ex:
 			if trycnt <= 0: print("Failed to retrieve")	
 			else: trycnt -= 1
-			time.sleep(0.05)
+		time.sleep(0.5)
 
 f.close()
